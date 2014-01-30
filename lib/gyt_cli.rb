8f97b6c6370d7ef16697ed355390def4642f624f
Joe Kurleto <joedoku@gmail.com>
2014-01-30 17:09:01 -0600

    Moved CLI to its own file

---
+++ lib/gyt_cli.rb
@@ -0,0 +1,39 @@
+require "gyt"
+
+class GytCLI < Thor
+  desc "init <pathspec>", "Initialize new Gyt repository"
+  def init
+    Gyt::Repository.init(current_dir)
+  end
+
+  desc "add <pathspec>", "Add file to index"
+  def add(filepath)
+    current_repo.add(filepath)
+  end
+
+  desc "status", "Show files to be committed"
+  def status
+    current_repo.status
+  end
+
+  desc "commit", "Commit staged files to history"
+  method_option :message, aliases: "-m", desc: "Commit message", banner: "<commit>", required: true
+  def commit
+    current_repo.commit!(options[:message])
+  end
+
+  desc "log", "Show commit history"
+  def log
+    current_repo.log
+  end
+
+  no_commands do
+    def current_dir
+      File.expand_path('.')
+    end
+
+    def current_repo
+      repo = Gyt::Repository.new(current_dir)
+    end
+  end
+end
